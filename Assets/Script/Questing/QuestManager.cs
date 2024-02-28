06ace31d984515f15e4a63da2febd0a92609ad15
ostmannen <60339614+ostmannen@users.noreply.github.com>
2024-02-28 21:24:03 +0100

    quest stuff

---
+++ Assets/Script/Questing/QuestManager.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestManager : MonoBehaviour
+{
+    private Dictionary<string, Quest> questMap;
+    private void Awake(){
+        questMap = CreateQuestMap();
+
+        Quest quest = GetQuestById("Test");
+        Debug.Log(quest.info.displayName);
+        Debug.Log(quest.info.levelRequirement);
+        Debug.Log(quest.state);
+        Debug.Log(quest.CurrentStepExists());
+    }
+    private Dictionary<String, Quest> CreateQuestMap()
+    {
+        
+        QuestInfoSO[] allQuest = Resources.LoadAll<QuestInfoSO>("Quest");
+
+        Dictionary<string, Quest> idToQuestMap = new Dictionary<string, Quest>();
+        foreach (QuestInfoSO questInfo in allQuest)
+        {
+            if (idToQuestMap.ContainsKey(questInfo.id))
+            {
+                Debug.LogWarning("Duplicate ID found when creating quest map" + questInfo.id);
+            }
+            idToQuestMap.Add(questInfo.id, new Quest(questInfo));
+        }
+        return idToQuestMap;
+    }
+    private Quest GetQuestById(string id){
+        Quest quest = questMap[id];
+        if (quest == null){
+            Debug.LogError("ID not found in the quest map: " + id);
+        }
+        return quest;
+    }
+}
